c781961da193f245f60ece43a957059c03566673
OthmaELbei <osamazayd1321@gmail.com>
2023-10-15 13:13:36 +0200

    Add nuw folder anout Link and Route And useContext

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,30 +1,55 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes, Link } from "react-router-dom";
-import Hello from "./Halle";
+import Hello from "./Hello";
 import Home from "./Home";
 import PostsList from "./PostsList";
+import PostDetails from "./PostDetails";
+import { postsContext } from "./contexts/postsContext";
+
 function App() {
+  let postsData = [
+    {
+      id: 1,
+      title: "Hello worde",
+      body: "ertyuio  oo",
+    },
+    {
+      id: 2,
+      title: "Hello WORDS",
+      body: "ertyuio f  oo",
+    },
+    {
+      id: 3,
+      title: "Hello firtS",
+      body: "ertyuio fg  oo",
+    },
+  ];
+
   return (
-    <div className="App">
-      <div>
-        <Link to="/hello">
-          <button style={{ fontSize: "23px" }}>Hello</button>
-        </Link>
+    <postsContext.Provider value={postsData}>
+      <div className="App">
+        <div>
+          <Link to="/hello">
+            <button style={{ fontSize: "23px" }}>Hello</button>
+          </Link>
+          <Link to={"/home"}>
+            <button style={{ fontSize: "23px" }}>home</button>
+          </Link>
+          <Link to="/posts">
+            <button style={{ fontSize: "23px" }}>pasts</button>
+          </Link>
+        </div>
 
-        <button style={{ fontSize: "23px" }}>home</button>
-        <Link to="/posts">
-          <button style={{ fontSize: "23px" }}>pasts</button>
-        </Link>
+        {/* Router */}
+        <Routes>
+          <Route path="/hello" element={<Hello />} />
+          <Route path="/posts" element={<PostsList />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/postDetails/:postId" element={<PostDetails />} />
+        </Routes>
       </div>
-
-      {/* Router */}
-      <Routes>
-        <Route path="/hello" element={<Hello />} />
-        <Route path="/posts" element={<PostsList />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    </postsContext.Provider>
   );
 }
 
